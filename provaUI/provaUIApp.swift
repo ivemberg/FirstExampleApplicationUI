a9dd8635f9e4a9589b97b8134a8492990113c7cb
i.bettini <i.bettini@gmail.com>
2022-06-21 12:38:28 +0200

    Initial Commit

---
+++ provaUI/provaUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  provaUIApp.swift
+//  provaUI
+//
+//  Created by Ivo Junior Bettini on 21/06/22.
+//
+
+import SwiftUI
+
+@main
+struct provaUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
